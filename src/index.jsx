0e2ef1f1a7c10233f9d6418b3f6dfb0bfa8aff99
Barbora Stundlova <b.stundlova@gmail.com>
2020-11-21 13:44:16 +0100

    first push

---
+++ src/index.jsx
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+import { render } from 'react-dom';
+import "./style.css"
+
+const App = () => {
+ return null
+};
+
+render(<App />, document.getElementById('app'));
